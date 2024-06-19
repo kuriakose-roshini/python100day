020aaddacec98b715f9cec4a31196b0cabc6f6bc
Roshini Kuriakose <134479658+kuriakose-roshini@users.noreply.github.com>
2024-06-19 23:07:18 +0530

    Create sumOfDigits.py
    day2

---
+++ sumOfDigits.py
@@ -0,0 +1,3 @@
+# this program will take an input of two digits and print the sum of the digits
+two_digit_number = input("enter a two digit no.")
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
