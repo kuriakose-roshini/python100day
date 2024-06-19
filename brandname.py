ee9adf1c96f2021532478262b09d6a4f1d08bdae
Roshini Kuriakose <134479658+kuriakose-roshini@users.noreply.github.com>
2024-06-19 09:39:23 +0530

    Create bandname.py
    day 1 of 100 days python

---
+++ brandname.py
@@ -0,0 +1,5 @@
+"""this program is to understand the concept of print() input() concatenation and some other basic things in python"""
+print("Welcome to the band name generator")
+city = input("which city did you grew up in?\n")
+pet = input("enter name of your pet\n")
+print("your band name could be "city + " " + pet)
