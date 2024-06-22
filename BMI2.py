39223e9563aac696375c39de589484022d21f77b
Roshini Kuriakose <134479658+kuriakose-roshini@users.noreply.github.com>
2024-06-22 11:50:36 +0530

    Create BMI2.py
    day 3 bmi 2.o

---
+++ BMI2.py
@@ -0,0 +1,17 @@
+""" upgraded  version of previous bmi"""
+
+height = float(input())
+
+weight = int(input())
+
+BMI = float(weight)/float(height)**
+if BMI < 18.5:
+  print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI < 25:
+  print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI < 30:
+  print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI < 35:
+  print(f"Your BMI is {BMI}, you are obese.")
+else:
+  print(f"Your BMI is {BMI}, you are clinically obese.")
