13c49f4ae15b7b53e86bbe0979c9d3cb448e8f33
Roshini Kuriakose <134479658+kuriakose-roshini@users.noreply.github.com>
2024-06-22 11:31:23 +0530

    Create OddEven.py
    day 3 of 100 day python

---
+++ OddEven.py
@@ -0,0 +1,10 @@
+"""this code is to check whether the number given is odd or even
+AIM: familiarize with if-else
+"""
+
+number = int(input())
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
