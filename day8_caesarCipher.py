7fd918128c84a0d04502972940a93b2ac26b48c3
Roshini Kuriakose <134479658+kuriakose-roshini@users.noreply.github.com>
2025-01-05 12:12:35 +0530

    Create day8_caesarCipher.py

---
+++ day8_caesarCipher.py
@@ -0,0 +1,36 @@
+
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+
+
+ 
+def caesar(original_text,shift_amount,encode_or_decode):
+    output_text = ""
+    if encode_or_decode == 'decode':
+            shift_amount *= -1
+
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+
+        
+    
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+should_continue = True
+
+while should_continue:
+    direction = input("Type encode to encrypt type decode for decrypt")
+    text = input("Type your msg:\n").lower()
+    shift=int(input("Type the shift no:\n"))
+    caesar(text,shift,direction)
+    restart=input("type 'yes if you want to go again else type no\n").lower()
+    if restart == "no":
+        should_continue=False
+        print("Goodbye")
+    
